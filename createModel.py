9e54bfdca6c54795b8f2a7a086c4bd708c521b7b
scarlettgrey <58540429+scarlettgrey@users.noreply.github.com>
2021-05-21 15:14:06 +0700

    updating some data and code

--- createModel.py
+++ createModel.py
@@ -1,3 +1,4 @@
+from os import access
 from geopy import distance
 from numpy.core.numeric import Inf
 import pandas as pd
@@ -6,7 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x_label = []
-with open('Label/T1R0.csv', 'r') as f:
+with open('Label/Label.csv', 'r') as f:
     f.readline()
     for a in f.readlines():
         a = int(a.strip('\n'))
@@ -18,39 +19,24 @@ with open('Label/T1R0.csv', 'r') as f:
                 temp.append(0)
         x_label.append(temp)
 
-ds = pd.read_csv('TestCase/TestCase1.csv')
-ds = ds.iloc[::, :].copy()
-
-randloc = pd.read_csv('RandLoc/RandomLongLat0.csv')
+randloc = pd.read_csv('RandLoc/RandomLongLat.csv')
 randloc = randloc.iloc[::, :].copy()
 
 x_train = []
-maxKamar = -Inf
-maxDokter = -Inf
-for a, b, c, d in zip(randloc['Lat'], randloc['Long'], ds['Sisa_Kamar'], ds['Dokter_Siaga']):
-    if maxKamar < c:
-        maxKamar = c
-    if maxDokter < d:
-        maxDokter = d
-    x_train.append([a, b, c, d])
-
-# Normalize the data
-# for a in range(len(x_train)):
-#     x_train[a][2] /= maxKamar
-#     x_train[a][3] /= maxDokter
+for a, b in zip(randloc['Lat'], randloc['Long']):
+    x_train.append([a, b])
 
 x_label = np.array(x_label, dtype=float)
 x_train = np.array(x_train, dtype=float)
 
 import tensorflow as tf
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Dense(64, input_shape=[4], activation='relu'),
+    tf.keras.layers.Dense(64, input_shape=[2], activation='relu'),
     tf.keras.layers.Dense(256, activation='relu'),
-    tf.keras.layers.Dense(units=len(x_label), activation='softmax')
-    # with normalize accuracy = 0.08
-    # without normalize accuracy = 0.17
+    tf.keras.layers.Dense(1024, activation='relu'),
+    tf.keras.layers.Dense(units=171, activation='softmax')
 ])
 
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-hist = model.fit(x_train, x_label, epochs=10)
+hist = model.fit(x_train, x_label, epochs=50)
